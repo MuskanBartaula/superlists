038604b90550e28002fcdfa54b0dcf34a22c56c3
Muskan Bartaula <muskan.bartaula109@gmail.com>
2023-06-11 10:04:59 +0545

    update: implement the test with unittest TestCase

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,14 +1,30 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+
 DRIVER_PATH = 'H:/Tutorial/Testing/lib/chromedriver.exe'
-service = Service(executable_path=DRIVER_PATH)
-chrome_options = Options()
-chrome_options.binary_location='C:/Program Files/Google/Chrome/Application/chrome.exe'
-chrome_options.add_argument('--no-sandbox')
+BINARY_PATH = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
-browser = webdriver.Chrome(service=service, options=chrome_options)
-browser.get('http://localhost:8000')
+class HomePageTest(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.service = Service(executable_path=DRIVER_PATH)
+        self.chrome_options = Options()
+        self.chrome_options.binary_location= BINARY_PATH
+        self.chrome_options.add_argument('--no-sandbox')
+        self.browser = webdriver.Chrome(service=self.service, options=self.chrome_options)
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+    
+    def test_home_page(self) -> None:
+        self.browser.get('http://localhost:8000')
+        assert 'The install worked successfully!' in self.browser.title, "The is not in title"
+    
 
-assert 'The' in browser.title, "The is not in title"
+if __name__ == '__main__':
+    unittest.main()
