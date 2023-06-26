27c891239142984622db8759aa56b5336db2cead
Muskan Bartaula <muskan.bartaula109@gmail.com>
2023-06-26 22:15:06 +0545

    multiple users with their own list url

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -6,10 +6,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 
 DRIVER_PATH = 'H:/Tutorial/Testing/lib/chromedriver.exe'
 BINARY_PATH = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
+MAX_WAIT = 10
 
 class HomePageTest(LiveServerTestCase):
     
@@ -25,11 +28,19 @@ class HomePageTest(LiveServerTestCase):
         self.browser.quit()
         return super().tearDown()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
-    
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+        
     def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         # Mukesh heard a cool todo list site on the internet. He visit the site via chrome.
         self.browser.get(self.live_server_url)
@@ -49,12 +60,48 @@ class HomePageTest(LiveServerTestCase):
         # He enter some to-do items
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        time.sleep(2)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make fly")
+
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make fly")
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith visit the site
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
         
-        # inputbox.send_keys('Use peacock feathers to make fly')
-        # inputbox.send_keys(Keys.ENTER)
-        # self.check_for_row_in_list_table("Use peacock feathers to make fly")
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         
+        self.browser.quit()
+        self.browser = webdriver.Chrome(service=self.service, options=self.chrome_options)
         
-        self.fail('Finish the test')
+        # Francis visit the site. There's no sign of Edith's content
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+        
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+
+        self.assertNotEqual(edith_list_url, francis_list_url)
+        
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNoIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+
